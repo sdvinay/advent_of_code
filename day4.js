6eefa209731c3f8cb5fbf658ecbe4e350c38e6f2
Vinay Kumar <sdvinay@gmail.com>
2015-12-07 20:04:21 -0800

    Day 4 Part 2 working: generalized function to take a string to match

--- day4.js
+++ day4.js
@@ -1,16 +1,17 @@
 var md5 = require('md5');
 
 
-function findLowestMatch(prefix) {
+function findLowestMatch(prefix, match) {
 	for (var i = 0; true; i++) {
 		var message = prefix + i.toString();
 		var hash = md5(message);
-		if (hash.indexOf('00000') === 0) {
+		if (hash.indexOf(match) === 0) {
 			return i;
 		}
 	}
 }
 
-console.log(findLowestMatch('abcdef'));
-console.log(findLowestMatch('pqrstuv'));
-console.log(findLowestMatch('bgvyzdsv'));
+console.log(findLowestMatch('abcdef', '00000'));
+console.log(findLowestMatch('pqrstuv', '00000'));
+console.log(findLowestMatch('bgvyzdsv', '00000'));
+console.log(findLowestMatch('bgvyzdsv', '000000'));
