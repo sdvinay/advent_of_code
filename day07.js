f2e0dca25af07d883375922f2b5f639f24eb9932
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-12-13 18:16:55 -0800

    Refactoring of day07

--- day07.js
+++ day07.js
@@ -1,6 +1,10 @@
+// Construct and evaluate a "circuit", based on instructions
+// Each instruction will become a function that lives in rules
+// Each rule is only evaluated once, and will memoize its value in values
 var rules = {};
 var values = {};
 
+// Regular expressions for the different types of instructions
 var reInt = /^(\d+) -> ([a-z]+)/;
 var reAndOrNum = /^(\d+) (AND|OR) ([a-z]+) -> ([a-z]+)/;
 var reAndOr = /^([a-z]+) (AND|OR) ([a-z]+) -> ([a-z]+)/;
@@ -8,6 +12,7 @@ var reNot = /^NOT ([a-z]+) -> ([a-z]+)/;
 var reShift = /^([a-z]+) (.SHIFT) (\d+) -> ([a-z]+)/;
 var rePipe = /^([a-z]+) -> ([a-z]+)/;
 
+// Get memoized if available, otherwise evaluate
 function get(name) {
 	if (name in values) {
 		return values[name];
@@ -16,68 +21,82 @@ function get(name) {
 	}
 }
 
+// An instructionProcessor is a regexp and the function that creates a rule from the matches
+var instructionProcessors = [ 
+	{re: reInt, fn: createIntRule},
+	{re: reAndOrNum, fn: createAndOrNumRule},
+	{re: reAndOr, fn: createAndOrRule},
+	{re: reNot, fn: createNotRule},
+	{re: rePipe, fn: createPipeRule},
+	{re: reShift, fn: createShiftRule}
+];
+
+// match the instruction to its type, and create the corresponding rule
 function processLine(str) {
-	var matches = str.match(reInt);
-	if (matches) {
-		var val = parseInt(matches[1]);
-		rules[matches[2]] = function() { return val;};
-		values[matches[2]] = val;
-		return;
-	}
-	matches = str.match(reAndOrNum);
-	if (matches) {
-		var v1 = parseInt(matches[1]);
-		var v2 = matches[3];
-		var name = matches[4];
-		if (matches[2] === 'OR') {
-			rules[name] = function() { var val = v1 | get(v2); values[name]=val; return val;};
+	for (var i in instructionProcessors) {
+		var proc = instructionProcessors[i];
+		var matches = str.match(proc.re);
+		if (matches) {
+			proc.fn(matches);
+			return;
 		}
-		if (matches[2] === 'AND') {
-			rules[name] = function() { var val = v1 & get(v2); values[name]=val; return val;};
-		}
-		return;
-	}
-	matches = str.match(reAndOr);
-	if (matches) {
-		var v1 = matches[1];
-		var v2 = matches[3];
-		var name = matches[4];
-		if (matches[2] === 'OR') {
-			rules[name] = function() { var val = get(v1) | get(v2); values[name]=val; return val;};
-		}
-		if (matches[2] === 'AND') {
-			rules[name] = function() { var val = get(v1) & get(v2); values[name]=val; return val;};
-		}
-		return;
-	}
-	matches = str.match(reNot);
-	if (matches) {
-		var v1 = matches[1];
-		var name = matches[2];
-		rules[name] = function() { var val = ((~get(v1))+(2<<15)); values[name]=val; return val;};
-		return;
-	}
-	matches = str.match(reShift);
-	if (matches) {
-		var v1 = matches[1];
-		var dist = matches[3];
-		var name = matches[4];
-		if (matches[2] === 'LSHIFT') {
-			rules[name] = function() { var val = get(v1) << dist; values[name] = val; return val; };
-		}
-		if (matches[2] === 'RSHIFT') {
-			rules[name] = function() { var val = get(v1) >> dist; values[name] = val; return val; };
-		}
-		return;
-	}
-	matches = str.match(rePipe);
-	if (matches) {
-		var v1 = matches[1];
-		var name = matches[2];
-		rules[name] = function() { var val = get(v1); values[name] = val; return val; };
-		return;
 	}
 	throw new Error('line didnt match pattern: '+str);
+}
+
+// These are all the create*Rule functions, for each instruction type
+function createIntRule(matches) {
+	var val = parseInt(matches[1]);
+	rules[matches[2]] = function() { return val;};
+	values[matches[2]] = val;
+}
+
+function createAndOrNumRule(matches) {
+	var v1 = parseInt(matches[1]);
+	var v2 = matches[3];
+	var name = matches[4];
+	if (matches[2] === 'OR') {
+		rules[name] = function() { var val = v1 | get(v2); values[name]=val; return val;};
+	}
+	if (matches[2] === 'AND') {
+		rules[name] = function() { var val = v1 & get(v2); values[name]=val; return val;};
+	}
+}
+
+function createAndOrRule(matches) {
+	var v1 = matches[1];
+	var v2 = matches[3];
+	var name = matches[4];
+	if (matches[2] === 'OR') {
+		rules[name] = function() { var val = get(v1) | get(v2); values[name]=val; return val;};
+	}
+	if (matches[2] === 'AND') {
+		rules[name] = function() { var val = get(v1) & get(v2); values[name]=val; return val;};
+	}
+}
+
+function createNotRule(matches) {
+	var v1 = matches[1];
+	var name = matches[2];
+	rules[name] = function() { var val = ((~get(v1))+(2<<15)); values[name]=val; return val;};
+}
+
+function createShiftRule(matches) {
+	var v1 = matches[1];
+	var dist = matches[3];
+	var name = matches[4];
+	if (matches[2] === 'LSHIFT') {
+		rules[name] = function() { var val = get(v1) << dist; values[name] = val; return val; };
+	}
+	if (matches[2] === 'RSHIFT') {
+		rules[name] = function() { var val = get(v1) >> dist; values[name] = val; return val; };
+	}
+}
+
+function createPipeRule(matches) {
+	var v1 = matches[1];
+	var name = matches[2];
+	rules[name] = function() { var val = get(v1); values[name]=val; return val;};
 }
 
 
