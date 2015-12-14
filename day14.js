2f149b5cd119db5413de2a5b5c39d12d16b37540
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-12-14 14:24:53 -0800

    Day 14 part 2 working

--- day14.js
+++ day14.js
@@ -1,10 +1,14 @@
 var endingTime = 2503;
 var re = /can fly (\d+) .* for (\d+) seconds, but then must rest for (\d+) seconds/;
+
+var reindeers = [];
 // match the instruction by regexp, and create the corresponding rule
 function processLine(str) {
 	var matches = str.match(re);
 	if (matches) {
-		console.log(countProgress(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]), endingTime));
+		var reindeer = { speed: parseInt(matches[1]), duration: parseInt(matches[2]), 
+			rest: parseInt(matches[3]), points: 0}; 
+		reindeers.push(reindeer);
 		return;
 	}
 	throw new Error('line didnt match pattern: '+str);
@@ -31,6 +35,28 @@ var rl = require('readline').createInterface({
 rl.on('line', processLine);
 
 rl.on('close', function () {
+		for (var i = 1; i <= endingTime; i++) {
+			var leadingScore = 0;
+			var leadingReindeer = [];
+			for (var j = 0; j < reindeers.length; j++) {
+				var score = countProgress(reindeers[j].speed, reindeers[j].duration, reindeers[j].rest, i);
+				if (score > leadingScore) {
+					leadingReindeer = [j];
+					leadingScore = score;
+				} else if (score == leadingScore) {
+					leadingReindeer.push(j);
+				}
+				if (i === endingTime) {
+					console.log(score);
+				}
+			}
+			for (var k = 0; k < leadingReindeer.length; k++) {
+				reindeers[leadingReindeer[k]].points++;
+			}
+		}
+		for (var j = 0; j < reindeers.length; j++) {
+			console.log(reindeers[j].points);
+		}
 });
 // */
 
