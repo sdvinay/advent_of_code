c0bd934a9d97d71d160634e3e557d0d56bc935e8
Vinay Kumar <sdvinay@gmail.com>
2015-12-14 19:58:49 -0800

    Day08 Part 2 working

--- day08.js
+++ day08.js
@@ -1,4 +1,4 @@
-function countStringSizeDiff(str) {
+function countStringSizeDiffDecode(str) {
 	// 2 for the wrapping quote marks
 	var origLen = str.length;
 	str = str.substr(1,str.length-2);
@@ -9,23 +9,34 @@ function countStringSizeDiff(str) {
 	str = str.replace(/\\x\w\w/g, 'X');
 	return origLen - str.length;
 }
+
+function countStringSizeDiffEncode(str) {
+	var origLen = str.length;
+	str = str.replace(/\\/g, '--');
+	str = str.replace(/"/g, '==');
+	return str.length+2 - origLen;
+}
 //* // literal/in-line test cases go here
 
 var testStrings = [ '""', '"abc"', '"aaa\\"aaa"', '"\\x27"'];
-console.log (testStrings.map(countStringSizeDiff)); // expect 2,2,3,5
+console.log (testStrings.map(countStringSizeDiffDecode)); // expect 2,2,3,5
+console.log (testStrings.map(countStringSizeDiffEncode)); // expect 2,2,3,5
 // */
 
 //*
 
-var diffCount = 0;
+var diffCountDecode = 0;
+var diffCountEncode = 0;
 function processLine(line) {
-	var count = countStringSizeDiff(line); 
-	diffCount+= count;
+	var count = countStringSizeDiffDecode(line); 
+	diffCountDecode+= count;
+	diffCountEncode+= countStringSizeDiffEncode(line);
 	console.log(count);
 }
 
 function onClose() {
-	console.log(diffCount);
+	console.log(diffCountEncode);
+	console.log(diffCountDecode);
 }
 var rl = require('readline').createInterface({
 	  input: require('fs').createReadStream('input/input08.txt'),
