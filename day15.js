f42dc76ad858c7dfa85185dd36e3124c17425167
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-12-15 14:49:21 -0800

    Day15 Part2 working

--- day15.js
+++ day15.js
@@ -1,7 +1,7 @@
-var ingr = [ [ 5, -1,  0,  0],
-			 [-1,  3,  0,  0],
-			 [ 0, -1,  4,  0],
-			 [-1,  0,  0,  2] ];
+var ingr = [ [ 5, -1,  0,  0, 5],
+			 [-1,  3,  0,  0, 1],
+			 [ 0, -1,  4,  0, 6],
+			 [-1,  0,  0,  2, 8] ];
 
 var numProperties = 4;
 function computeScore(ingrs, counts) {
@@ -18,7 +18,15 @@ function computeScore(ingrs, counts) {
 	return totalScore;
 }
 
-console.log(computeScore(ingr, [44, 56]));
+function computeCalories(ingrs, counts) {
+	var calories = 0;
+	for (var j = 0; j < counts.length; j++) {
+		calories += counts[j]*ingr[j][4]
+		
+	}
+	return calories;
+}
+
 
 var totalCookies = 100;
 var highScore = 0;
@@ -27,9 +35,11 @@ for (var i = 0; i <= totalCookies; i++) {
 		for (var k = 0; k <= totalCookies - i - j; k++) {
 			var remainTotal = totalCookies - i - j - k;
 			counts = [i, j, k, remainTotal];
-			var score = computeScore(ingr, counts);
-			console.log(counts, score);
-			if (score > highScore) { highScore = score; }
+			if (computeCalories(ingr, counts) === 500) {
+				var score = computeScore(ingr, counts);
+				console.log(counts, score);
+				if (score > highScore) { highScore = score; }
+			}
 		}
 	}
 }
