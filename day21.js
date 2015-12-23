ff1dbcf6930eed56b06f2789d5fabb7b40e9e1bc
Vinay Kumar <sdvinay@gmail.com>
2015-12-22 17:57:06 -0800

    day21 par1 working

--- day21.js
+++ day21.js
@@ -19,17 +19,29 @@ function playFight(p1, p2) {
 
 weapons = [ [8, 4], [10, 5], [25, 6], [40, 7], [74, 8] ];
 armors  = [ [0, 0], [13, 1], [31, 2], [53, 3], [75, 4], [102, 5] ];
+rings   = [ [0,0,0], [25,1,0], [50,2,0], [100,3,0], [20,0,1], [40,0,2], [80,0,3]];
 function enumeratePlayers() {
 	var players = [];
-	var player = {hit:100, armor:0, cost:0, damage:0};
 	for (var i = 0; i < weapons.length; i++) {
-		player.cost += weapons[i][0];
-		player.damage = weapons[i][1];
 		for (var j = 0; j < armors.length; j++) {
-			player.cost += armors[j][0];
-			player.armor = armors[j][1];
-			players.push(JSON.parse(JSON.stringify(player)));
-			var player = {hit:100, armor:0, cost:0, damage:0};
+			for (var k = 0; k < rings.length; k++) {
+				for (var l = 0; l < rings.length; l++) {
+					var player = {hit:100, armor:0, cost:0, damage:0};
+					player.cost += weapons[i][0];
+					player.damage = weapons[i][1];
+					player.cost += armors[j][0];
+					player.armor = armors[j][1];
+					players.push(player);
+					if (k==l && k>0)
+						continue;
+					player.cost += rings[k][0];
+					player.damage += rings[k][1];
+					player.armor  += rings[k][2];
+					player.cost += rings[l][0];
+					player.damage += rings[l][1];
+					player.armor  += rings[l][2];
+				}
+			}
 		}
 	}
 	return players;
