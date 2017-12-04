91655a39b63249ae2135016a15a1a06c2e1751f9
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2017-12-03 23:21:56 -0800

    2017 Day 3, computeRingDepth() working

--- 2017_03.js
+++ 2017_03.js
@@ -10,18 +10,35 @@
 //        Other is distance from the middle of the side
 
 function computeDistance(num) {
-	return 0;
+	var ringDepth =  computeRingDepth(num);
+	var ringSideLength = ringDepth+1;
+	var stepsIntoRing = num - (ringDepth * ringDepth);
+	var stepsFromCorner = stepsIntoRing % ringSideLength;
+	var stepsFromCenterOfRing = Math.abs((ringSideLength/2) - stepsFromCorner);
+
+	return stepsFromCenterOfRing;
+}
+
+function computeRingDepth(num) {
+	if (num === 1) { return 0; }
+	var largestSqrt = Math.floor(Math.sqrt(num-1));
+	var oddSqrt = largestSqrt % 2 == 1 ? largestSqrt : largestSqrt - 1;
+	return (oddSqrt+1)/2;
 }
 
 function test(num) {
-	console.log(num, computeDistance(num));
+	console.log(num, computeRingDepth(num), computeDistance(num));
 }
 
 input = 325489;
 
 test(1);
+test(2);
+test(9);
 test(12);
 test(23);
+test(25);
+test(26);
 test(1024);
 test(input);
 
