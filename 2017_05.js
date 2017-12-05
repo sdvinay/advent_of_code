70738767d7000933eb91140ffe313890ef2d4eeb
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2017-12-05 11:06:43 -0800

    Day 5, Part 2 Completed

--- 2017_05.js
+++ 2017_05.js
@@ -1,11 +1,13 @@
 //* // literal/in-line test cases go here
 // */
 
-console.log(execMaze([0, 3, 0, 1, -3]));
+console.log(execMaze([0, 3, 0, 1, -3], false)); // expect 5
+console.log(execMaze([0, 3, 0, 1, -3], true));  // expect 10
 
 //*
 
-function execMaze(maze) {
+
+function execMaze(maze, PartTwo) {
 	var steps = 0;
 	var index = 0;
 	var mazeSize = maze.length;
@@ -13,21 +15,26 @@ function execMaze(maze) {
 	while (index >= 0 && index < mazeSize) {
 		steps++;
 		var offset = maze[index];
-		maze[index]++;
+		var adjustment = (PartTwo == true && offset >= 3) ? -1 : +1;
+		maze[index]+= adjustment;
 		index += offset;
 	}
 	
 	return steps;
 }
 
-var maze = [];
+// Two copies of the maze, for Part 1 and Part 2
+var maze1 = [];
+var maze2 = [];
 
 function processLine(line) {
-	maze.push(parseInt(line));
+	maze1.push(parseInt(line));
+	maze2.push(parseInt(line));
 }
 
 function onClose() {
-	console.log(execMaze(maze));
+	console.log(execMaze(maze1, false));
+	console.log(execMaze(maze2, true));
 }
 var rl = require('readline').createInterface({
 	  input: require('fs').createReadStream('input/input2017_05.txt'),
