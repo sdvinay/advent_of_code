f4c695cf470dcfee5f9f3342113bd86f65d9df45
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2017-12-06 17:16:48 -0800

    Day 5 (both completed) use enums instead of bools

--- 2017_05.js
+++ 2017_05.js
@@ -1,13 +1,18 @@
+var Problem = { 
+	part1 : 1,
+	part2 : 2
+};
+
 //* // literal/in-line test cases go here
 // */
 
-console.log(execMaze([0, 3, 0, 1, -3], false)); // expect 5
-console.log(execMaze([0, 3, 0, 1, -3], true));  // expect 10
+console.log(execMaze([0, 3, 0, 1, -3], Problem.part1)); // expect 5
+console.log(execMaze([0, 3, 0, 1, -3], Problem.part2));  // expect 10
 
 //*
 
 
-function execMaze(maze, PartTwo) {
+function execMaze(maze, problemPart) {
 	var steps = 0;
 	var index = 0;
 	var mazeSize = maze.length;
@@ -15,7 +20,7 @@ function execMaze(maze, PartTwo) {
 	while (index >= 0 && index < mazeSize) {
 		steps++;
 		var offset = maze[index];
-		var adjustment = (PartTwo === true && offset >= 3) ? -1 : +1;
+		var adjustment = (problemPart === Problem.part2 && offset >= 3) ? -1 : +1;
 		maze[index] += adjustment;
 		index += offset;
 	}
@@ -33,8 +38,8 @@ function processLine(line) {
 }
 
 function onClose() {
-	console.log(execMaze(maze1, false));
-	console.log(execMaze(maze2, true));
+	console.log(execMaze(maze1, Problem.part1));
+	console.log(execMaze(maze2, Problem.part2));
 }
 var rl = require('readline').createInterface({
 	  input: require('fs').createReadStream('input/input2017_05.txt'),
