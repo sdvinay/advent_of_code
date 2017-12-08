17ad50c5a7b2669a48e018f8afeb4b22047cac49
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2017-12-07 19:36:34 -0800

    Day 07, Part 1 completed

---
+++ 2017_07.js
@@ -0,0 +1,44 @@
+//* // literal/in-line test cases go here
+// */
+
+//*
+
+// For part 1, we are looking for the root node
+// This is the only node that won't appear on the RHS of any rule
+// So just enumerate the nodes, and the nodes that appear on the RHSs,
+// and find the one node that isn't in the RHS of any rule.
+
+var programs = [];
+var children = [];
+
+function processLine(line) {
+	var left = line.match(/(^\w+) /)[1];
+	programs.push(left);
+
+	var match = line.match(/-> (.*)/);
+	if (match) {
+		children.push(...match[1].split(', '));
+	}
+}
+
+function onClose() {
+	console.log(programs);
+	console.log(children);
+
+	for (i in programs) {
+		if (!children.includes(programs[i])) {
+			console.log(programs[i]);
+			break;
+		}
+	}
+}
+var rl = require('readline').createInterface({
+	  input: require('fs').createReadStream('input/input2017_07.txt'),
+	  output: process.stdout,
+	  terminal: false
+  });
+rl.on('line', processLine);
+rl.on('close', onClose);
+
+// */
+
