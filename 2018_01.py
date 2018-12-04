a54460b727c76441cfefaf9c5c729762da72dcfe
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2018-12-04 13:59:16 -0800

    2018 Day 01, part 2

--- 2018_01.py
+++ 2018_01.py
@@ -1,8 +1,19 @@
 import fileinput
 
 frequency = 0
-for line in fileinput.input():
-	frequency += int(line)
+seen_frequencies = {}
+with open('input/input2018_01.txt') as f:
+	lines = f.readlines()
+duplicate_seen = 0
+while not duplicate_seen:
+	for line in lines:
+		frequency += int(line)
+		if frequency in seen_frequencies:
+			print "Duplicate frequency observed " + str(frequency)
+			duplicate_seen = 1
+			break
+		seen_frequencies[frequency] = 1;
+
 
 print frequency
 
