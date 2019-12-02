6e1afad99a5e02faf27133e459d8bd08ddb30623
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-02 15:10:13 -0800

    Some cleanup of Day 2, in prep for part 2

--- 2019_02.py
+++ 2019_02.py
@@ -1,6 +1,5 @@
 
-def run_program(inputstr):
-	program = [int(x) for x in (inputstr.split(','))]
+def run_program(program):
 	position = 0
 	while program[position] != 99:
 		opcode = program[position]
@@ -12,12 +11,17 @@ def run_program(inputstr):
 		val = (arg1+arg2, arg1*arg2)[opcode-1]
 		program[dest] = val
 		position += 4
-	print program
+	return program
+
+def program_from_str(inputstr):
+	program = [int(x) for x in (inputstr.split(','))]
+	return program
+
+print(run_program(program_from_str("1,0,0,0,99")))
+print(run_program(program_from_str("2,3,0,3,99")))
+print(run_program(program_from_str("2,4,4,5,99,0")))
+print(run_program(program_from_str("1,1,1,4,99,5,6,0,99")))
 
 with open('input/input_2019_02.txt') as f:
-	run_program(f.read())
+	print run_program(program_from_str(f.read()))
 
-run_program("1,0,0,0,99")
-run_program("2,3,0,3,99")
-run_program("2,4,4,5,99,0")
-run_program("1,1,1,4,99,5,6,0,99")
