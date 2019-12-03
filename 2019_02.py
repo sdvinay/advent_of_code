ec58fb7249d3049e9a637c2d860e84721265590a
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-02 22:54:27 -0800

    Day 2: code readability

--- 2019_02.py
+++ 2019_02.py
@@ -1,29 +1,33 @@
+TEST_INPUTS=["1,0,0,0,99", "2,3,0,3,99", "2,4,4,5,99,0", "1,1,1,4,99,5,6,0,99"]
+INPUT_FILE='input/input_2019_02.txt'
+TARGET_OUTPUT=19690720
 
 def run_program(program):
-	position = 0
-	while program[position] != 99:
-		opcode = program[position]
-		src1 = program[position+1]
-		src2 = program[position+2]
-		dest = program[position+3]
+	instr_ptr = 0
+	while program[instr_ptr] != 99:
+		opcode = program[instr_ptr]
+		src1 = program[instr_ptr+1]
+		src2 = program[instr_ptr+2]
+		dest = program[instr_ptr+3]
 		arg1 = program[src1]
 		arg2 = program[src2]
 		val = (arg1+arg2, arg1*arg2)[opcode-1]
 		program[dest] = val
-		position += 4
+		instr_ptr += 4
 	return program
 
 def program_from_str(inputstr):
 	program = [int(x) for x in (inputstr.split(','))]
 	return program
 
-print(run_program(program_from_str("1,0,0,0,99")))
-print(run_program(program_from_str("2,3,0,3,99")))
-print(run_program(program_from_str("2,4,4,5,99,0")))
-print(run_program(program_from_str("1,1,1,4,99,5,6,0,99")))
+def test():
+	for input in TEST_INPUTS:
+		print(run_program(program_from_str(input)))
+
+test()
 
 original_program = []
-with open('input/input_2019_02.txt') as f:
+with open(INPUT_FILE) as f:
 	original_program = program_from_str(f.read()) 
 	
 program = list(original_program)
@@ -35,6 +39,6 @@ for noun in range(99):
 		program[1] = noun
 		program[2] = verb
 		output = run_program(program)[0]
-		if output == 19690720:
+		if output == TARGET_OUTPUT:
 			print (100*noun+verb)
 
