5bf605b712ae3794fe1fd6ccacb77d7f8a47cb8e
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-02 18:30:12 -0800

    Day 1: Code cleanup/simplification

--- 2019_01.py
+++ 2019_01.py
@@ -1,34 +1,34 @@
-def fuel_required(mass):
+# fuel burned to transport mass (Part 1)
+def fuel_burned(mass):
 	if mass < 6: return 0
 	return int(mass/3)-2
 
-def total_fuel_required(mass):
+# fuel_required accounts iteratively for including the fuel in the mass (Part 2)
+def fuel_required(mass):
 	total_fuel = 0
-	fuel = fuel_required(mass)
+	fuel = fuel_burned(mass)
 	while (fuel>0):
 		total_fuel += fuel
-		fuel = fuel_required(fuel)
+		fuel = fuel_burned(fuel)
 	return total_fuel
 
-def test_case(input):
-	print(fuel_required(input), total_fuel_required(input))
-
+# these are the test inputs provided in the problem
+# TODO: Actually check the outputs, rather than just print them
 def test():
-	test_case(4)
-	test_case(12)
-	test_case(14)
-	test_case(1969)
-	test_case(100756)
+	for input in [4, 12, 14, 1969, 100756]:
+		print(fuel_burned(input), fuel_required(input))
 
 test()
 
+# The instructions explicitly say to compute fuel requirements on an item-by-item
+# basis, (including on fuel amounts in Part 2) rather than on total mass :shrug:
 total1 = 0
 total2 = 0
 with open('input/input_2019_01.txt') as f:
 	lines = f.readlines()
 for line in lines:
-	total1 += fuel_required(int(line))
-	total2 += total_fuel_required(int(line))
+	total1 += fuel_burned(int(line))
+	total2 += fuel_required(int(line))
 
 print(total1, total2)
 
