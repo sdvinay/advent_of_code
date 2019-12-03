b43f2e3bad25e853132e3e6736d115d646e683a5
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-02 18:45:31 -0800

    Day 1: More code cleanup/simplification

--- 2019_01.py
+++ 2019_01.py
@@ -1,3 +1,6 @@
+INPUT_FILE='input/input_2019_01.txt'
+TEST_INPUTS=[4, 12, 14, 1969, 100756] #inputs provided in problem
+
 # fuel burned to transport mass (Part 1)
 def fuel_burned(mass):
 	return ((int(mass/3)-2) if mass >= 6 else 0)
@@ -14,7 +17,7 @@ def fuel_required(mass):
 # these are the test inputs provided in the problem
 # TODO: Actually check the outputs, rather than just print them
 def test():
-	for input in [4, 12, 14, 1969, 100756]:
+	for input in TEST_INPUTS:
 		print(fuel_burned(input), fuel_required(input))
 
 test()
@@ -23,7 +26,7 @@ test()
 # basis (including on fuel amounts in Part 2), rather than on total mass :shrug:
 total1 = 0
 total2 = 0
-with open('input/input_2019_01.txt') as f:
+with open(INPUT_FILE) as f:
 	lines = f.readlines()
 for line in lines:
 	total1 += fuel_burned(int(line))
