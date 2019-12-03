d93014edf58c9bca1805f37297aca9f024725c2a
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-03 00:45:45 -0800

    Day 3: Part 2 completed

--- 2019_03.py
+++ 2019_03.py
@@ -1,28 +1,28 @@
 from sets import Set
 
 INPUT_FILE='input/input_2019_03.txt'
-
-unit_vectors = { 'R': (1, 0), 'L': (-1, 0), 'D': (0, -1), 'U': (0, 1)}
+UNIT_VECTORS = { 'R': (1, 0), 'L': (-1, 0), 'D': (0, -1), 'U': (0, 1)}
 
 def points_in_path(path):
 	position = (0, 0)
 	step_count = 0
 	points = {}
 	for vector in path.split(","):
-		direction = unit_vectors[vector[0]]
+		direction = UNIT_VECTORS[vector[0]]
 		distance = int(vector[1:])
 		for i in range(distance):
 			position = (position[0]+direction[0], position[1]+direction[1])
-			step_count++
+			step_count+= 1
 			points[position]=step_count
 	return points
 		
 def find_nearest_collision(path1, path2):
-	p1 = keys(points_in_path(path1))
-	p2 = keys(points_in_path(path2))
-	collisions = p1 & p2
+	p1 = points_in_path(path1)
+	p2 = points_in_path(path2)
+	collisions = Set(p1.keys()) & Set(p2.keys())
 	dists = [abs(pt[0])+abs(pt[1]) for pt in collisions]
-	return min(dists)
+	steps = [p1[pt]+p2[pt] for pt in collisions]
+	return min(dists), min(steps)
 
 
 print find_nearest_collision("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")
