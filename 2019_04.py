53efc3f6b098b6dddbbb7ab23d06b7d2db5ee2be
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-03 21:25:00 -0800

    Day 4: Part 1 completed

---
+++ 2019_04.py
@@ -0,0 +1,27 @@
+TEST_INPUTS=[111111, 223450, 123789]
+INPUT_START=347312
+INPUT_END=805915
+
+def has_repeat(input):
+	password = str(input)
+	match = any([password[i] == password[i+1] for i in range(len(password)-1)])
+	return match
+
+def non_decreasing(input):
+	password = str(input)
+	return all([password[i] <= password[i+1] for i in range(len(password)-1)])
+
+def test():
+	for input in TEST_INPUTS:
+		print(has_repeat(input), non_decreasing(input))
+
+test()
+
+matching = 0
+for i in range(INPUT_START, INPUT_END):
+	if (has_repeat(i) and non_decreasing(i)): matching += 1
+
+print matching
+
+
+
