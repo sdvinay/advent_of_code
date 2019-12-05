2ccdcaa1019b2ed9e908ded578fd12a106d6362c
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-04 21:04:26 -0800

    Day 4: Completed part 2; improved code consistency/readability

--- 2019_04.py
+++ 2019_04.py
@@ -1,35 +1,36 @@
 TEST_INPUTS=[111111, 223450, 123789, 112233, 123444, 111122]
 INPUT_RANGE=[347312, 805915]
 
-def streaks(input):
+# count repeating digits (e.g., find "streaks")
+def get_streak_lengths(input):
 	password = str(input)
-	streaks = []
+	lengths = []
 	current_streak = 1
-	for ptr in range(len(password)-1):
-		if password[ptr] == password[ptr+1]:
+	for i in range(len(password)-1):
+		if password[i] == password[i+1]:
 			current_streak += 1
 		else:
-			streaks.append(current_streak)
+			lengths.append(current_streak)
 			current_streak = 1
-	streaks.append(current_streak)
-	return streaks
+	lengths.append(current_streak)
+	return lengths
 
 
-def non_decreasing(input):
+def is_non_decreasing(input):
 	password = str(input)
 	return all([password[i] <= password[i+1] for i in range(len(password)-1)])
 
 def test():
 	for input in TEST_INPUTS:
-		print(input, non_decreasing(input), streaks(input))
+		print(input, is_non_decreasing(input), get_streak_lengths(input))
 
 test()
 
 matching_1 = 0
 matching_2 = 0
-for passwd in range(INPUT_RANGE[0], INPUT_RANGE[1]):
-	if non_decreasing(passwd):
-		my_streaks = streaks(passwd)
+for password in range(INPUT_RANGE[0], INPUT_RANGE[1]):
+	if is_non_decreasing(password):
+		my_streaks = get_streak_lengths(password)
 		if any(s>1  for s in my_streaks): matching_1 += 1
 		if any(s==2 for s in my_streaks): matching_2 += 1
 
