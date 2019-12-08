10b595671f486195f036f85d3963773bc4863e23
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-08 13:12:05 -0800

    Day 7 part 1 completed

--- 2019_07.py
+++ 2019_07.py
@@ -1,9 +1,10 @@
+import itertools
 import intcode_machine
 
 TEST_INPUTS=[
-	([4,3,2,1,0], "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", 43210),
-	([0,1,2,3,4], "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", 54321),
-	([1,0,4,3,2], "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", 65210)
+	((4,3,2,1,0), "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", 43210),
+	((0,1,2,3,4), "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", 54321),
+	((1,0,4,3,2), "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", 65210)
    	]
 NUM_PHASES=5
 INPUT_FILE='input/input_2019_07.txt'
@@ -17,13 +18,10 @@ def run_circuit(program_str, phase_sequence):
 
 	return next_input
 
-SEQUENCES = []
-for input in TEST_INPUTS:
-	SEQUENCES.append(input[0])
-
 def find_max_phase_sequence(program_str):
 	highest_output = 0
 	best_seq_seen = None
+	SEQUENCES = itertools.permutations(range(NUM_PHASES))
 	for seq in SEQUENCES:
 		output = run_circuit(program_str, seq)
 		if (output > highest_output):
