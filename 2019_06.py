666ba8923d5bd10fb83e29bc2c8539ce14c526fc
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-07 20:18:18 -0800

    Day 6, part 2 complete, improved readability

--- 2019_06.py
+++ 2019_06.py
@@ -7,23 +7,26 @@ with open(INPUT_FILE) as f:
 		values = line.rstrip().split(')')
 		orbits[values[1]]=values[0]
 	
-def find_orbits(planet):
+def find_orbit_path(planet):
 	if planet == 'COM': return []
 	inner = orbits[planet]
-	val = [inner] + find_orbits(inner)
+	val = [inner] + find_orbit_path(inner)
 	return val
 
 total_orbits=0
 for planet in orbits.keys():
-	orbs = find_orbits(planet)
+	orbs = find_orbit_path(planet)
 	print (planet, orbs)
 	total_orbits += len(orbs)
 print(total_orbits)
 
-path1 = set(find_orbits('YOU'))
-path2 = set(find_orbits('SAN'))
+def find_common_delta(item1, item2):
+	path1 = set(find_orbit_path(item1))
+	path2 = set(find_orbit_path(item2))
 
-uni = path1.union(path2)
-inters = path1.intersection(path2)
-print (len(uni)-len(inters))
+	uni = path1.union(path2)
+	inters = path1.intersection(path2)
+	return (len(uni)-len(inters))
+
+print(find_common_delta('YOU', 'SAN'))
 
