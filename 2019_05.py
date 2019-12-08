765dfad1349cc40a213691415da89f6bb36daf12
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2019-12-08 12:04:46 -0800

    Day 7 partial; improve the Intcode computer and the tests

--- 2019_05.py
+++ 2019_05.py
@@ -1,26 +1,48 @@
 TEST_INPUTS=[
-	(8, "3,9,8,9,10,9,4,9,99,-1,8"),
-	(8, "3,9,7,9,10,9,4,9,99,-1,8"),
-	(8, "3,3,1108,-1,8,3,4,3,99"  ),
-    (8, "3,3,1107,-1,8,3,4,3,99"  ),
-	(0, "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"),
-    (0,	"3,3,1105,-1,9,1101,0,0,12,4,12,99,1"),
-	(7, "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99") ]
+	([8], "3,9,8,9,10,9,4,9,99,-1,8", [1]),   # ==8
+	([7], "3,9,8,9,10,9,4,9,99,-1,8", [0]),   # ==8
+	([8], "3,9,7,9,10,9,4,9,99,-1,8", [0]),   # <8
+	([9], "3,9,7,9,10,9,4,9,99,-1,8", [0]),   # <8
+	([7], "3,9,7,9,10,9,4,9,99,-1,8", [1]),   # <8
+	([8], "3,3,1108,-1,8,3,4,3,99",   [1]),   # ==8
+	([7], "3,3,1108,-1,8,3,4,3,99"  , [0]),   # ==8
+    ([8], "3,3,1107,-1,8,3,4,3,99"  , [0]),   # <8
+    ([9], "3,3,1107,-1,8,3,4,3,99"  , [0]),   # <8
+    ([7], "3,3,1107,-1,8,3,4,3,99",   [1]),   # <8
+
+# The following couple programs have this behavior:
+#output 0 if the input was zero or 1 if the input was non-zero
+	([0], "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", [0]), 
+	([1], "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", [1]),
+	([-1], "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", [1]),
+    ([0],	"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", [0]),
+    ([1],	"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", [1]),
+    ([-1],	"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", [1]),
+
+# output 999 if the input value is below 8, 
+# output 1000 if the input value is equal to 8, or 
+#output 1001 if the input value is greater than 8.
+	([7], "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [999]),
+	([8], "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [1000]),
+	([9], "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [1001]),
+   	]
 INPUT_FILE='input/input_2019_05.txt'
 
 # these are the operations of the diagnostic machine
 # for each op code, define the number of inputs, number of outputs, and the function to run on the inputs
 # For outputs, -1 is a magic code meaning that the instruction pointer jumps to the output param if the returned value is true
 operations = {
-	1: (2,  1, lambda args: args[1]+args[2]),
-	2: (2,  1, lambda args: args[1]*args[2]),
-	3: (0,  1, lambda args: args[0]),
-	4: (1,  0, lambda args: args[1]),
-	5: (2, -1, lambda args: 1 if args[1]<>0 else 0),
-	6: (2, -1, lambda args: 1 if args[1]==0 else 0),
-	7: (2,  1, lambda args: 1 if args[1]< args[2] else 0),
-	8: (2,  1, lambda args: 1 if args[1]==args[2] else 0) }
+	1: (2,  1, lambda args: args[1]+args[2]), # sum
+	2: (2,  1, lambda args: args[1]*args[2]), # multiply
+	3: (0,  1, lambda args: args[0].pop(0)),  # input
+	4: (1,  0, lambda args: args[1]),         # output
+	5: (2, -1, lambda args: 1 if args[1]<>0 else 0),  # jump-if-true
+	6: (2, -1, lambda args: 1 if args[1]==0 else 0),  # jump-if-false
+	7: (2,  1, lambda args: 1 if args[1]< args[2] else 0),   # less-than
+	8: (2,  1, lambda args: 1 if args[1]==args[2] else 0) }  # equals
 
+# prog_input is a list of inputs to be used by instruction 3 (input).
+# returns a list of values outputted by instruction 4 (output)
 def run_program(prog_input, program):
 	instr_ptr = 0
 	output_value = []
@@ -62,9 +84,11 @@ def program_from_str(inputstr):
 	return program
 
 def test():
-	for (inp, program_str) in TEST_INPUTS:
+	for (inp, program_str, expected_output) in TEST_INPUTS:
 		print(inp, program_str)
-		print(run_program(inp, program_from_str(program_str)))
+		output = (run_program(inp, program_from_str(program_str)))
+		print (expected_output, output, expected_output==output)
+	
 
 test()
 
@@ -73,6 +97,6 @@ with open(INPUT_FILE) as f:
 	original_program = program_from_str(f.read()) 
 	
 program = list(original_program)
-print(run_program(5, program))
+print(run_program([5], program))
 
 
